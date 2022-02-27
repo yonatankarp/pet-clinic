f5432caadd50cf289bfd417dd1c3d46aaf0c3e4a
Yonatan Karp-Rudin <yonvata@gmail.com>
2022-02-27 10:59:20 +0100

    Add Vets List and update controller (Closes #14)

--- pet-clinic-web/src/main/java/com/yonatankarp/petclinic/contorllers/VetController.java
+++ pet-clinic-web/src/main/java/com/yonatankarp/petclinic/contorllers/VetController.java
@@ -1,13 +1,23 @@
 package com.yonatankarp.petclinic.contorllers;
 
+import com.yonatankarp.petclinic.services.VetService;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 @RequestMapping("/vets")
 public class VetController {
+
+    private final VetService vetService;
+
+    public VetController(VetService vetService) {
+        this.vetService = vetService;
+    }
+
     @RequestMapping({"", "/", "/index", "/index.html"})
-    public String listVets() {
+    public String listVets(final Model model) {
+        model.addAttribute("vets", vetService.findAll());
         return "vets/index";
     }
 }
