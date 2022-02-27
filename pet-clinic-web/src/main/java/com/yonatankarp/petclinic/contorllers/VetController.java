f3748d24758168d7fef72c5dfb6614b599d41836
Yonatan Karp-Rudin <yonvata@gmail.com>
2022-02-27 08:50:48 +0100

    Add vet page and controller (Closes #7)

---
+++ pet-clinic-web/src/main/java/com/yonatankarp/petclinic/contorllers/VetController.java
@@ -0,0 +1,13 @@
+package com.yonatankarp.petclinic.contorllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/vets")
+public class VetController {
+    @RequestMapping({"", "/", "/index", "/index.html"})
+    public String listVets() {
+        return "vets/index";
+    }
+}
