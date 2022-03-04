b58f78fdf4ea644dba4be9560f353937e9f3778b
Yonatan Karp-Rudin <yonvata@gmail.com>
2022-03-04 07:57:29 +0100

    Add Pet Type mapped based service. (Closes #27)

---
+++ pet-clinic-data/src/main/java/com/yonatankarp/petclinic/services/PetTypeService.java
@@ -0,0 +1,6 @@
+package com.yonatankarp.petclinic.services;
+
+import com.yonatankarp.petclinic.model.PetType;
+
+public interface PetTypeService extends CrudService<PetType, Long> {
+}
---
+++ pet-clinic-data/src/main/java/com/yonatankarp/petclinic/services/map/PetTypeServiceMap.java
@@ -0,0 +1,34 @@
+package com.yonatankarp.petclinic.services.map;
+
+import java.util.Set;
+import com.yonatankarp.petclinic.model.PetType;
+import com.yonatankarp.petclinic.services.PetTypeService;
+import org.springframework.stereotype.Service;
+
+@Service
+public class PetTypeServiceMap extends AbstractMapService<PetType, Long>  implements PetTypeService{
+    @Override
+    public Set<PetType> findAll() {
+        return super.findAll();
+    }
+
+    @Override
+    public void deleteById(final Long id) {
+        super.deleteById(id);
+    }
+
+    @Override
+    public void delete(final PetType petType) {
+        super.delete(petType);
+    }
+
+    @Override
+    public PetType save(final PetType petType) {
+        return super.save(petType);
+    }
+
+    @Override
+    public PetType findById(final Long id) {
+        return super.findById(id);
+    }
+}
