ade6630669b09c3104ae03cd9daf5eae2ec83f4d
Yonatan Karp-Rudin <yonvata@gmail.com>
2022-03-04 23:12:54 +0100

    Convert Vet and Specialty to JPA entities (Closes #35)

--- pet-clinic-data/src/main/java/com/yonatankarp/petclinic/model/Specialty.java
+++ pet-clinic-data/src/main/java/com/yonatankarp/petclinic/model/Specialty.java
@@ -1,13 +1,14 @@
 package com.yonatankarp.petclinic.model;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Table;
+import lombok.Data;
+
+@Data
+@Entity
+@Table(name = "specialties")
 public class Specialty extends BaseEntity {
+    @Column(name = "description")
     private String description;
-
-    public String getDescription() {
-        return description;
-    }
-
-    public void setDescription(String description) {
-        this.description = description;
-    }
 }
--- pet-clinic-data/src/main/java/com/yonatankarp/petclinic/model/Vet.java
+++ pet-clinic-data/src/main/java/com/yonatankarp/petclinic/model/Vet.java
@@ -1,16 +1,22 @@
 package com.yonatankarp.petclinic.model;
 
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.JoinColumn;
+import javax.persistence.JoinTable;
+import javax.persistence.ManyToMany;
+import javax.persistence.Table;
 import java.util.HashSet;
 import java.util.Set;
+import lombok.Data;
 
+@Data
+@Entity
+@Table(name = "vets")
 public class Vet extends Person {
+    @ManyToMany(fetch = FetchType.EAGER)
+    @JoinTable(name = "vet_specialties",
+            joinColumns = @JoinColumn(name = "vat_id"),
+            inverseJoinColumns = @JoinColumn(name = "specialty_id"))
     private Set<Specialty> specialties = new HashSet<>();
-
-    public Set<Specialty> getSpecialties() {
-        return specialties;
-    }
-
-    public void setSpecialties(Set<Specialty> specialties) {
-        this.specialties = specialties;
-    }
 }
