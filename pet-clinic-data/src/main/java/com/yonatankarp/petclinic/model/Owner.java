b4e9153b50f40d42fa0448508233861177ff9ff1
Yonatan Karp-Rudin <yonvata@gmail.com>
2022-03-04 09:17:55 +0100

    Disable lombok

--- pet-clinic-data/src/main/java/com/yonatankarp/petclinic/model/Owner.java
+++ pet-clinic-data/src/main/java/com/yonatankarp/petclinic/model/Owner.java
@@ -1,16 +1,43 @@
 package com.yonatankarp.petclinic.model;
 
+import java.util.HashSet;
 import java.util.Set;
-import lombok.Data;
-import lombok.EqualsAndHashCode;
-import lombok.NoArgsConstructor;
 
-@Data
-@NoArgsConstructor
-@EqualsAndHashCode(callSuper = true)
 public class Owner extends Person {
     private String address;
     private String city;
     private String phone;
-    private Set<Pet> pets;
+    private Set<Pet> pets = new HashSet<>();
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public Set<Pet> getPets() {
+        return pets;
+    }
+
+    public void setPets(Set<Pet> pets) {
+        this.pets = pets;
+    }
 }
--- pet-clinic-data/src/main/java/com/yonatankarp/petclinic/model/Pet.java
+++ pet-clinic-data/src/main/java/com/yonatankarp/petclinic/model/Pet.java
@@ -1,15 +1,42 @@
 package com.yonatankarp.petclinic.model;
 
 import java.time.LocalDate;
-import lombok.Data;
-import lombok.EqualsAndHashCode;
-import lombok.NoArgsConstructor;
 
-@Data
-@NoArgsConstructor
-@EqualsAndHashCode(callSuper = true)
 public class Pet extends BaseEntity {
+    private String name;
     private PetType petType;
     private Owner owner;
     private LocalDate birthDate;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public PetType getPetType() {
+        return petType;
+    }
+
+    public void setPetType(PetType petType) {
+        this.petType = petType;
+    }
+
+    public Owner getOwner() {
+        return owner;
+    }
+
+    public void setOwner(Owner owner) {
+        this.owner = owner;
+    }
+
+    public LocalDate getBirthDate() {
+        return birthDate;
+    }
+
+    public void setBirthDate(LocalDate birthDate) {
+        this.birthDate = birthDate;
+    }
 }
