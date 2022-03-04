3320d51945cfbef11fc7e6096713ae7d79c1606f
Yonatan Karp-Rudin <yonvata@gmail.com>
2022-03-04 10:51:16 +0100

    Fix broken links in the menu (Closes #43)

--- pet-clinic-web/src/main/java/com/yonatankarp/petclinic/contorllers/VetController.java
+++ pet-clinic-web/src/main/java/com/yonatankarp/petclinic/contorllers/VetController.java
@@ -6,7 +6,7 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
-@RequestMapping("/vets")
+@RequestMapping
 public class VetController {
 
     private final VetService vetService;
@@ -15,7 +15,7 @@ public class VetController {
         this.vetService = vetService;
     }
 
-    @RequestMapping({"", "/", "/index", "/index.html"})
+    @RequestMapping({ "/vets", "/vets/index", "/vets/index.html", "/vets.html"})
     public String listVets(final Model model) {
         model.addAttribute("vets", vetService.findAll());
         return "vets/index";
