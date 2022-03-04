adef60d596a2e3b80ba8f033f50a141a6686c8e7
Yonatan Karp-Rudin <yonvata@gmail.com>
2022-03-04 09:29:30 +0100

    Add Specialties map service implementation. (Closes #29)

---
+++ pet-clinic-data/src/main/java/com/yonatankarp/petclinic/services/SpecialtiesService.java
@@ -0,0 +1,6 @@
+package com.yonatankarp.petclinic.services;
+
+import com.yonatankarp.petclinic.model.Specialty;
+
+public interface SpecialtiesService extends CrudService<Specialty, Long> {
+}
---
+++ pet-clinic-data/src/main/java/com/yonatankarp/petclinic/services/map/SpecialtiesServiceMap.java
@@ -0,0 +1,34 @@
+package com.yonatankarp.petclinic.services.map;
+
+import java.util.Set;
+import com.yonatankarp.petclinic.model.Specialty;
+import com.yonatankarp.petclinic.services.SpecialtiesService;
+import org.springframework.stereotype.Service;
+
+@Service
+public class SpecialtiesServiceMap extends AbstractMapService<Specialty, Long> implements SpecialtiesService {
+    @Override
+    public Set<Specialty> findAll() {
+        return super.findAll();
+    }
+
+    @Override
+    public void deleteById(final Long id) {
+        super.deleteById(id);
+    }
+
+    @Override
+    public Specialty save(final Specialty specialty) {
+        return super.save(specialty);
+    }
+
+    @Override
+    public void delete(final Specialty specialty) {
+        super.delete(specialty);
+    }
+
+    @Override
+    public Specialty findById(final Long id) {
+        return super.findById(id);
+    }
+}
